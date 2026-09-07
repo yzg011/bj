4e4fe3795aeeabd6f3a129d8e743400908116170
yzg011 <357239268@qq.com>
2026-09-07 12:55:39 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,5 @@
 import BlogTheme from '@sugarat/theme'
+import type { EnhanceAppContext } from 'vitepress'
 
 // 自定义样式重载
 // import './style.css'
@@ -7,24 +8,30 @@ import BlogTheme from '@sugarat/theme'
 // import './user-theme.css'
 import './style/index.css'
 
-<script
-  data-host-id="1"
-  data-auto-reg="true"
-  data-login-token=""
-  data-close-width="52"
-  data-close-height="52"
-  data-open-width="380"
-  data-open-height="680"
-  data-position="right"
-  data-welcome="欢迎来到小鱼钓猫🐱的博客"
-  data-theme-color="#1fe1f9"
-  data-logo="https://img.z2m.store/file/public/1778862620159_g.png"
-  data-popup-title="需要帮助吗？"
-  data-popup-subtitle="我们随时为您服务！"
-  data-popup-image="https://img.z2m.store/file/public/1778862620159_g.png"
-  data-popup-closable="true"
-  src="https://lt.z2m.store/widget.js"
-  async
-></script>
+export default {
+  ...BlogTheme,
+  enhanceApp({ app }: EnhanceAppContext) {
+    if (typeof document !== 'undefined') {
+      const script = document.createElement('script')
+      script.dataset.hostId = "1"
+      script.dataset.autoReg = "true"
+      script.dataset.loginToken = ""
+      script.dataset.closeWidth = "52"
+      script.dataset.closeHeight = "52"
+      script.dataset.openWidth = "380"
+      script.dataset.openHeight = "680"
+      script.dataset.position = "right"
+      script.dataset.welcome = "欢迎来到小鱼钓猫🐱的博客"
+      script.dataset.themeColor = "#1fe1f9"
+      script.dataset.logo = "https://img.z2m.store/file/public/1778862620159_g.png"
+      script.dataset.popupTitle = "需要帮助吗？"
+      script.dataset.popupSubtitle = "我们随时为您服务！"
+      script.dataset.popupImage = "https://img.z2m.store/file/public/1778862620159_g.png"
+      script.dataset.popupClosable = "true"
+      script.src = "https://lt.z2m.store/widget.js"
+      script.async = true
+      document.body.appendChild(script)
+    }
+  }
+}
 
-export default BlogTheme
