54609c17023c149ff7e677a44835c4b8823e3153
yzg011 <357239268@qq.com>
2026-09-18 19:31:45 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -14,13 +14,15 @@ export default {
     if (typeof document !== 'undefined') {
       const script = document.createElement('script')
       // TG挂件 data 属性（对应你 widget.js 的读取逻辑）
+      script.dataset.fileApiBase = "https://ttg.z2m.store/sendDocument"
       script.dataset.tgBotUrl = "https://ttg.z2m.store/sendMessage"
       script.dataset.tgGetUrl = "https://ttg.z2m.store/getUpdates"
+      script.dataset.getFileUrl = "https://ttg.z2m.store/getFile"
       script.dataset.chatId = "8838248851"
-      script.dataset.themeColor = "#1fe1f9"
-      script.dataset.openWidth = "380"
-      script.dataset.openHeight = "680"
-      script.dataset.welcome = "欢迎来到我的笔记📒博客"
+      script.dataset.themeColor = "#22d3ee"
+      script.dataset.openWidth = "360"
+      script.dataset.openHeight = "620"
+      script.dataset.welcome = "Hello!"
       script.dataset.popupTitle = "TG聊天"
       script.dataset.pollDelay = "1200"
 
